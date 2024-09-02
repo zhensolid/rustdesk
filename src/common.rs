75809f9251b2de0a93f813977cbd340143ba4135
zhensolid <30351491+zhensolid@users.noreply.github.com>
2024-09-02 16:56:55 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://139.185.50.105:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
